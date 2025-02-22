0e23fa57e8330759982062f96e59d2289917f972
mparnell <parnm@farmingdale.edu>
2025-02-21 19:16:05 -0500

    commit: logo added, homepage started

--- BlabApp/app/src/main/java/com/example/blabapp/Screens/SplashScreen.kt
+++ BlabApp/app/src/main/java/com/example/blabapp/Screens/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.example.blabapp.Screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,21 +9,24 @@ import androidx.compose.ui.Alignment
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.res.painterResource
+import com.example.blabapp.R
 import com.example.blabapp.ui.theme.BlabPurple
-import com.example.blabapp.ui.theme.BlabYellow
 
 
 @Composable
 fun SplashScreen(navController: NavController) {
+    val logoPic = painterResource(R.drawable.logo)
     Box(
         modifier = Modifier.fillMaxSize().background(BlabPurple),
         contentAlignment = Alignment.Center
     ) {
-        Text("Blab App", fontSize = 50.sp, color = BlabYellow)
+        Image(
+            painter = logoPic,
+            contentDescription = null
+        )
     }
 
     LaunchedEffect(Unit) {
--- BlabApp/app/src/main/java/com/example/blabapp/ui/theme/Theme.kt
+++ BlabApp/app/src/main/java/com/example/blabapp/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BlabGrey,
+    secondary = BlabPurple,
+    tertiary = BlabBlue
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = BlabYellow,
+    secondary = BlabPurple,
+    tertiary = BlabGreen
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
