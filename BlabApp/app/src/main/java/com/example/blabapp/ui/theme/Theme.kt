73d84ba53c9acd4469b687fbcc13af7c2304d5a5
Osmany A Argueta Claros <osmarguet@gmail.com>
2025-04-22 10:43:50 -0400

    theme update

--- BlabApp/app/src/main/java/com/example/blabapp/ui/theme/Theme.kt
+++ BlabApp/app/src/main/java/com/example/blabapp/ui/theme/Theme.kt
@@ -9,8 +9,13 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource.Companion.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = BlabPurple,
@@ -34,6 +39,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 
+
 @Composable
 fun BlabAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -42,6 +48,17 @@ fun BlabAppTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+    val view = LocalView.current
+    val activity = view.context as Activity
+
+    SideEffect {
+        val window = activity.window
+        // Change the system nav bar color to your theme's primary color
+        window.navigationBarColor = colorScheme.primary.toArgb()
+
+        // Make nav bar icons dark or light depending on theme
+        WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = !darkTheme
+    }
 
     MaterialTheme(
         colorScheme = colorScheme,
